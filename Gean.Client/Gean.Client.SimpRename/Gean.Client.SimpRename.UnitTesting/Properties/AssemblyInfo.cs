585734f3ab8cf95c222907b5df1b196df278d27b
simpkan <simpkan@7a404b14-5196-11de-9d99-59ef8d702e28>
2009-07-10 05:51:30 +0000

    刚学会如何查找一串“####”的正则

--- Gean.Client/Gean.Client.SimpRename/Gean.Client.SimpRename.UnitTesting/Properties/AssemblyInfo.cs
+++ Gean.Client/Gean.Client.SimpRename/Gean.Client.SimpRename.UnitTesting/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列属性集
 // 控制。更改这些属性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Gean.Client.SimpRename")]
+[assembly: AssemblyTitle("Gean.Client.SimpRename.UnitTesting")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("nsimple.cn")]
-[assembly: AssemblyProduct("Gean.Client.SimpRename")]
+[assembly: AssemblyProduct("Gean.Client.SimpRename.UnitTesting")]
 [assembly: AssemblyCopyright("Copyright © nsimple.cn 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,19 +20,16 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("3af16f76-1ebc-43a1-bca3-49167c86c5a4")]
+[assembly: Guid("6ce8b8b3-ebc2-4279-a0fe-76f7275bbafb")]
 
 // 程序集的版本信息由下面四个值组成:
 //
 //      主版本
-//      次版本 
+//      次版本
 //      内部版本号
 //      修订号
 //
-// 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
+// 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
 // 方法是按如下所示使用“*”:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Gean.Client.SimpRename.UnitTesting")]
