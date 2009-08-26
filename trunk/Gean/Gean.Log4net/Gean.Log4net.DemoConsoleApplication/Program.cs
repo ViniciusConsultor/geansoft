49f165732b07ee02a18008c644ccdf06f88ef0c9
simpkan <simpkan@7a404b14-5196-11de-9d99-59ef8d702e28>
2009-08-26 16:35:34 +0000

    花了一下午，半晚上，大致将Log4net的基本用法搞明白了。并将其做了基本的封装，可以使用更方便一些。 新建Gean.Log4net程序集对log4net进行封装，当然现在还是比较简单，但基本上够调试使用。

---
+++ trunk/Gean/Gean.Log4net/Gean.Log4net.DemoConsoleApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gean.Log4net.DemoConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log4netLoggingService log = Log4netLoggingService.Initialize(); 
+            log.Debug(Guid.NewGuid());
+            log.Error(Guid.NewGuid());
+            log.Fatal(Guid.NewGuid());
+            log.Info(Guid.NewGuid());
+            log.Warn(Guid.NewGuid());
+            Console.ReadKey();
+            log.Debug(Guid.NewGuid());
+            log.Error(Guid.NewGuid());
+            log.Fatal(Guid.NewGuid());
+            log.Info(Guid.NewGuid());
+            log.Warn(Guid.NewGuid());
+            Console.ReadKey();
+            log.Debug(Guid.NewGuid());
+            log.Error(Guid.NewGuid());
+            log.Fatal(Guid.NewGuid());
+            log.Info(Guid.NewGuid());
+            log.Warn(Guid.NewGuid());
+            Console.ReadKey();
+            log.Debug(Guid.NewGuid());
+            log.Error(Guid.NewGuid());
+            log.Fatal(Guid.NewGuid());
+            log.Info(Guid.NewGuid());
+            log.Warn(Guid.NewGuid());
+            Console.ReadKey();
+        }
+    }
+}
